f199752c91cf5189fc113fad1a3f7346e27f4655
sychen <sychen@ctrip.com>
2024-05-06 17:42:52 +0800

    [CELEBORN-1409] CommitHandler commitFiles RPC supports separate timeout configuration
    ### What changes were proposed in this pull request?
    This PR aims to supports separate timeout configuration at CommitHandler commitFiles RPC.
    
    ### Why are the changes needed?
    The default value of `celeborn.worker.commitFiles.timeout` is 120s, and the default value of Client's RPC is 60s.
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    GA
    
    Closes #2488 from cxzl25/CELEBORN-1409.
    
    Authored-by: sychen <sychen@ctrip.com>
    Signed-off-by: SteNicholas <programgeek@163.com>
    
    (cherry picked from commit dc5219216331be1ab0b949f681542452a9c465b1)

--- client/src/main/scala/org/apache/celeborn/client/commit/CommitHandler.scala
+++ client/src/main/scala/org/apache/celeborn/client/commit/CommitHandler.scala
@@ -69,6 +69,7 @@ abstract class CommitHandler(
     val sharedRpcPool: ThreadPoolExecutor) extends Logging {
 
   private val pushReplicateEnabled = conf.clientPushReplicateEnabled
+  private val clientRpcCommitFilesAskTimeout = conf.clientRpcCommitFilesAskTimeout
 
   private val commitEpoch = new AtomicLong()
   private val totalWritten = new LongAdder
@@ -458,7 +459,7 @@ abstract class CommitHandler(
           message.replicaIds)
       }(ec)
     } else {
-      worker.endpoint.ask[CommitFilesResponse](message)
+      worker.endpoint.ask[CommitFilesResponse](message, clientRpcCommitFilesAskTimeout)
     }
   }
 
