938a3d2d0e04af00694d23fa22abcc2b71f7bf05
王馨雨 <xinyuwang1@xiaohongshu.com>
2024-11-04 11:29:55 +0000

    Revert "Merge branch 'celeborn_1386' into 'red-branch-0.4'"
    This reverts merge request !35

--- worker/src/main/java/org/apache/celeborn/service/deploy/worker/shuffledb/RocksDBProvider.java
+++ worker/src/main/java/org/apache/celeborn/service/deploy/worker/shuffledb/RocksDBProvider.java
@@ -22,14 +22,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 
-import org.apache.commons.io.FileUtils;
-import org.rocksdb.BlockBasedTableConfig;
-import org.rocksdb.BloomFilter;
-import org.rocksdb.CompressionType;
-import org.rocksdb.InfoLogLevel;
-import org.rocksdb.Options;
-import org.rocksdb.RocksDBException;
-import org.rocksdb.Status;
+import org.rocksdb.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -74,7 +67,7 @@ public class RocksDBProvider {
       } catch (RocksDBException e) {
         if (e.getStatus().getCode() == Status.Code.NotFound) {
           logger.info("Creating state database at " + dbFile);
-          createIfMissing(dbOptions, dbFile);
+          dbOptions.setCreateIfMissing(true);
           try {
             tmpDb = org.rocksdb.RocksDB.open(dbOptions, dbFile.toString());
           } catch (RocksDBException dbExc) {
@@ -84,21 +77,21 @@ public class RocksDBProvider {
           // the RocksDB file seems to be corrupt somehow.  Let's just blow it away and create
           // a new one, so we can keep processing new apps
           logger.error(
-              "Error opening rocksdb file {}. Creating new file, will not be able to "
+              "error opening rocksdb file {}. Creating new file, will not be able to "
                   + "recover state for existing applications",
               dbFile,
               e);
           if (dbFile.isDirectory()) {
             for (File f : Objects.requireNonNull(dbFile.listFiles())) {
               if (!f.delete()) {
-                logger.warn("Error deleting {}", f.getPath());
+                logger.warn("error deleting {}", f.getPath());
               }
             }
           }
           if (!dbFile.delete()) {
-            logger.warn("Error deleting {}", dbFile.getPath());
+            logger.warn("error deleting {}", dbFile.getPath());
           }
-          createIfMissing(dbOptions, dbFile);
+          dbOptions.setCreateIfMissing(true);
           try {
             tmpDb = org.rocksdb.RocksDB.open(dbOptions, dbFile.toString());
           } catch (RocksDBException dbExc) {
@@ -115,19 +108,6 @@ public class RocksDBProvider {
       }
     }
     return tmpDb;
-  }
-
-  private static void createIfMissing(Options dbOptions, File dbFile) {
-    logger.info("Creating database file {} if missing", dbFile);
-    dbOptions.setCreateIfMissing(true);
-    // RocksDB does not support creating non-existent multi-level directory.
-    if (!dbFile.exists()) {
-      try {
-        FileUtils.forceMkdir(dbFile);
-      } catch (IOException e) {
-        logger.warn("Failed to create database file {}", dbFile, e);
-      }
-    }
   }
 
   private static class RocksDBLogger extends org.rocksdb.Logger {
@@ -150,28 +130,28 @@ public class RocksDBProvider {
    * Minor version differences are allowed -- meaning we should be able to read dbs that are either
    * earlier *or* later on the minor version.
    */
-  public static void checkVersion(org.rocksdb.RocksDB db, StoreVersion newVersion)
+  public static void checkVersion(org.rocksdb.RocksDB db, StoreVersion newversion)
       throws IOException, RocksDBException {
     byte[] bytes = db.get(StoreVersion.KEY);
     if (bytes == null) {
-      storeVersion(db, newVersion);
+      storeVersion(db, newversion);
     } else {
       ArrayList<Integer> versions = PbSerDeUtils.fromPbStoreVersion(bytes);
       StoreVersion version = new StoreVersion(versions.get(0), versions.get(1));
-      if (version.major != newVersion.major) {
+      if (version.major != newversion.major) {
         throw new IOException(
-            "Cannot read state DB with version "
+            "cannot read state DB with version "
                 + version
                 + ", incompatible "
                 + "with current version "
-                + newVersion);
+                + newversion);
       }
-      storeVersion(db, newVersion);
+      storeVersion(db, newversion);
     }
   }
 
   public static void storeVersion(org.rocksdb.RocksDB db, StoreVersion version)
-      throws RocksDBException {
+      throws IOException, RocksDBException {
     db.put(StoreVersion.KEY, PbSerDeUtils.toPbStoreVersion(version.major, version.minor));
   }
 }
--- worker/src/test/java/org/apache/celeborn/service/deploy/worker/shuffledb/DBProviderSuiteJ.java
+++ worker/src/test/java/org/apache/celeborn/service/deploy/worker/shuffledb/DBProviderSuiteJ.java
@@ -23,29 +23,14 @@ import static org.junit.Assume.assumeFalse;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.UUID;
 
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
 import org.apache.celeborn.common.util.JavaUtils;
 import org.apache.celeborn.common.util.Utils;
 
-@RunWith(Parameterized.class)
 public class DBProviderSuiteJ {
-
-  @Parameter public boolean createDirectory;
-
-  @Parameters(name = "createDirectory: {0}")
-  public static Collection<Object> data() {
-    return Arrays.asList(new Object[] {true, false});
-  }
 
   @Test
   public void testRockDBCheckVersionFailed() throws IOException {
@@ -59,11 +44,8 @@ public class DBProviderSuiteJ {
   }
 
   private void testCheckVersionFailed(DBBackend dbBackend, String namePrefix) throws IOException {
-    File dbDir = new File(System.getProperty("java.io.tmpdir"), namePrefix);
-    File dbFile =
-        createDirectory
-            ? Utils.createDirectory(dbDir.getPath(), namePrefix)
-            : new File(dbDir.getPath(), String.format("%s-%s", namePrefix, UUID.randomUUID()));
+    String root = System.getProperty("java.io.tmpdir");
+    File dbFile = Utils.createDirectory(root, namePrefix);
     try {
       StoreVersion v1 = new StoreVersion(1, 0);
       DBProvider.initDB(dbBackend, dbFile, v1).close();
@@ -72,7 +54,7 @@ public class DBProviderSuiteJ {
           assertThrows(IOException.class, () -> DBProvider.initDB(dbBackend, dbFile, v2));
       assertTrue(ioe.getMessage().contains("incompatible with current version StoreVersion[2.0]"));
     } finally {
-      JavaUtils.deleteRecursively(dbDir);
+      JavaUtils.deleteRecursively(dbFile);
     }
   }
 }
