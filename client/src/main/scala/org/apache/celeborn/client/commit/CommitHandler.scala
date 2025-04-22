6cc08e80160110d0cb763ff5928a1398a24bc754
xinyuwang1 <xinyuwang1@xiaohongshu.com>
2025-04-22 19:54:37 +0800

    修改commitfiles日志位置

--- client/src/main/scala/org/apache/celeborn/client/commit/CommitHandler.scala
+++ client/src/main/scala/org/apache/celeborn/client/commit/CommitHandler.scala
@@ -312,9 +312,9 @@ abstract class CommitHandler(
             case scala.util.Success(res) =>
               res.status match {
                 case StatusCode.SUCCESS | StatusCode.PARTIAL_SUCCESS | StatusCode.SHUFFLE_NOT_REGISTERED | StatusCode.REQUEST_FAILED | StatusCode.WORKER_EXCLUDED =>
-                  logDebug(s"Request commitFiles return ${res.status} for " +
-                    s"${Utils.makeShuffleKey(appUniqueId, shuffleId)}")
                   if (res.status != StatusCode.SUCCESS && res.status != StatusCode.WORKER_EXCLUDED) {
+                    logWarning(s"Request commitFiles return ${res.status} for " +
+                      s"${Utils.makeShuffleKey(appUniqueId, shuffleId)}")
                     commitFilesFailedWorkers.put(worker, (res.status, System.currentTimeMillis()))
                   }
                   processResponse(res, worker)
